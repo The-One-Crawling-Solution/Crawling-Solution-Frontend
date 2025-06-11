75106010e9bb8f0ff2c09ad10f0779b2871e70e1
Urvesh Radadiya <urveshradadiya1121@gmail.com>
2025-06-12 00:43:00 +0530

    ✨ ADD: Seo Related Changes

--- src/Components/seo/HelmetWrapper.js
+++ src/Components/seo/HelmetWrapper.js
@@ -1,4 +1,3 @@
-import React from "react";
 import { Helmet } from "react-helmet";
 import { useLocation } from "react-router-dom";
 import seoConfig from "./seoConfig";
@@ -15,13 +14,32 @@ const HelmetWrapper = () => {
   return (
     <Helmet>
       <title>{seoData.title}</title>
-      <meta name="description" content={seoData.description} />
-      <meta name="description" content={seoData.description} />
+      <meta property="og:locale" content="en_US" />
+      <meta property="og:type" content="website" />
       <meta property="og:title" content={seoData.title} />
       <meta property="og:description" content={seoData.description} />
       <meta property="og:url" content={window.location.href} />
-      <meta property="og:type" content="website" />
+      <meta property="og:site_name" content="The One Crawling Solution" />
+      <meta
+        property="og:image"
+        content="assets/images/slider/WebScraping.jpeg"
+      />
+
       <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={seoData.title} />
+      <meta name="twitter:url" content={window.location.href} />
+      <meta name="twitter:description" content={seoData.description} />
+      <meta
+        name="twitter:image"
+        content="assets/images/slider/WebScraping.jpeg"
+      />
+      <meta
+        name="twitter:image:alt"
+        content="Best web scraping service in USA | The One Crawling Solution"
+      />
+      <meta name="twitter:site" content="@onecrawling6263" />
+      <meta name="twitter:creator" content="@onecrawling6263" />
+      <link rel="canonical" href="https://theonecrawlingsolution.com/" />
     </Helmet>
   );
 };
