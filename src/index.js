f32cd2a893187f54d54a5a845a351f5da8f57551
Urvesh Radadiya <urveshradadiya1121@gmail.com>
2025-06-15 12:15:01 +0530

    ✨ ADD: CI/CD with domain management

--- src/index.js
+++ src/index.js
@@ -6,12 +6,12 @@ import HelmetWrapper from "./Components/seo/HelmetWrapper";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const branch = window.location.hostname.includes("develop") ? "/develop" : "/";
+// const branch = window.location.hostname.includes("develop") ? "/develop" : "/";
 
 root.render(
   <BrowserRouter
     future={{ v7_startTransition: true, v7_relativeSplatPath: true }}
-    basename={branch}
+    // basename={branch}
   >
     <HelmetWrapper />
     <App />
